3725f563fa029f25074be51b2e95832c3970e8c8
H82M19\Student <Student>
2025-04-02 11:28:34 +0700

    react2thang4

---
+++ my-app/src/components/UserTable.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router,Routes,Routed,NavLink} from 'react-router-dom'
+import Home from "./pages/Home"
+import Admin from "./pages/Admin"
+import {Container,Nav,Navbar} from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function UserTable(){
+    return(
+        <Router>
+            <Navbar className="bg bg-black">
+
+            </Navbar>
+        </Router>
+    )
+}
